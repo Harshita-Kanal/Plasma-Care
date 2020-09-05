b6d6e24dd28f1ecefd224bbc14fb55e3fe56bb97
Harshita-Kanal <harshita.kgv@gmail.com>
2020-09-05 23:16:18 +0530

    Basic urls, about and faq pages

--- plasma/urls.py
+++ plasma/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home)
+    path('', views.home, name = 'home'),
+    path('faq/', views.faq, name = 'faq'),
+    path('donor/', views.donor, name='donor'),
+    path('patient/', views.patient, name='patient'),
+    path('citizen/', views.citizen, name = 'citizen'),
+    path('about/', views.about, name='about')
+
 ]
--- plasma/views.py
+++ plasma/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
 def home(request):
     return render(request, 'plasma/main.html')
+
+
+def faq(request):
+    return render(request, 'plasma/faq.html')
+
+
+def donor(request):
+    return render(request, 'plasma/donor.html')
+
+
+def patient(request):
+    return render(request, 'plasma/patient.html')
+
+
+def about(request):
+    return render(request, 'plasma/About.html')
+
+
+def citizen(request):
+    return render(request, 'plasma/citizen.html')
 # Create your views here.
